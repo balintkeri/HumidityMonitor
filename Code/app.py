744480f46d89dec5b67d0988eb00c3952f5d3559
balintkeri <balintkeri0515@gmail.com>
2025-12-16 21:49:39 +0100

    Datahandler added

--- Code/app.py
+++ Code/app.py
@@ -1,3 +1,13 @@
 
-import streamlit as st
 
+from collections import deque
+
+LENGHT = 3 * 24 * 12 # 3 days of data at 5-minute intervals
+
+class DataHandler:
+    def __init__(self, length:int = 100 ):
+        self.lenght = length
+        self.readData()
+
+    def readData(self):
+        pass
