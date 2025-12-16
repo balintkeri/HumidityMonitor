e7df5161f083671f8eb04b628c507797a9e18c55
balintkeri <balintkeri0515@gmail.com>
2025-12-16 21:56:54 +0100

    writeData

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,6 @@
+import csv
 
-
+from datetime import datetime
 from collections import deque
 
 LENGHT = 3 * 24 * 12 # 3 days of data at 5-minute intervals
@@ -10,4 +11,21 @@ class DataHandler:
         self.readData()
 
     def readData(self):
-        pass
+        self.data = deque(maxlen=self.lenght)
+
+    def writeData(self):
+        with open('data.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["TimeStamp", "Humidity", "Temperature"])
+            for row in self.data:
+                print(row)
+                writer.writerow(row)
+
+    def appendData(self, humidity, temperature):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.data.append([timestamp, humidity, temperature])
+
+db = DataHandler(LENGHT)
+
+db.appendData(50.2, 23.4)
+db.writeData()
